fd31e5c9c18477c29ca44cf86db186f43e63f4dc
OhYeah! <hidemioya@gmail.com>
2026-04-28 11:59:46 +0900

    Add unzip commands for acis.zip and spec.zip

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+$datapath = dirname(__DIR__) . '/data';
+exec("unzip -d /tmp $datapath/acis.zip");
+exec("unzip -d /tmp $datapath/spec.zip");
 chdir(dirname(__DIR__) . '/macs');
 require_once "./index.php";
 ?>
