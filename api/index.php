fdef5d6f5abff5eff66f295419bea583f80ad84a
OhYeah! <hidemioya@gmail.com>
2026-05-01 10:01:56 +0900

    Modify index.php to copy database files instead of unzip
    Commented out the unzip commands and added file copy operations.

--- api/index.php
+++ api/index.php
@@ -1,7 +1,10 @@
 <?php
-chdir(dirname(__DIR__) . '/data');
-echo exec("unzip acis.zip -d /tmp");
-echo exec("unzip spec.zip -d /tmp");
+//chdir(dirname(__DIR__) . '/data');
+//echo exec("unzip acis.zip -d /tmp");
+//echo exec("unzip spec.zip -d /tmp");
+$datadir = __DIR__ . '/data;
+copy("$datadir/acis.db", "/tmp/acis.db");
+copy("$datadir/spec.db", "/tmp/spec.db");
 chdir(dirname(__DIR__) . '/macs');
 require_once "./index.php";
 ?>
