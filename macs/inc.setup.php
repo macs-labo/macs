1829977d1fc90c2c498b167ea27b9b0fd6f8443e
OhYeah! <hidemioya@gmail.com>
2026-05-01 09:40:06 +0900

    Update inc.setup.php

--- macs/inc.setup.php
+++ macs/inc.setup.php
@@ -31,10 +31,8 @@ $home = array('url' => $url, 'title' => $sitename.$branch);
 $isVercel = (getenv('VERCEL') === '1');
 if ($isVercel) {
   $user_root = '';
-  $dbPath = __DIR__ . '/data';
-  //echo $dbPath;
-  //$dbPath = '.data';
-  //$dbPath = '/tmp';
+  //$dbPath = __DIR__ . '/data';
+  $dbPath = '/tmp';
   $smartyPath = dirname(__DIR__) . '/vendor/smarty/smarty/libs/';
   $Debug = false; // Vercel は基本 false
 } else {
