5ca4aebd53b7f3d21c36b1761b9490dc39dc487a
OhYeah! <hidemioya@gmail.com>
2026-05-01 09:23:39 +0900

    Disable data extraction commands in index.php
    Comment out data extraction commands for acis.zip and spec.zip.

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-chdir(dirname(__DIR__) . '/data');
-exec("unzip acis.zip -d /tmp");
-exec("unzip spec.zip -d /tmp");
+//chdir(dirname(__DIR__) . '/data');
+//exec("unzip acis.zip -d /tmp");
+//exec("unzip spec.zip -d /tmp");
 chdir(dirname(__DIR__) . '/macs');
 require_once "./index.php";
 ?>
