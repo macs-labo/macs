06b0c2d0a66b8f0928c98ab7d720d46ce19285ee
OhYeah! <hidemioya@gmail.com>
2026-05-01 10:34:29 +0900

    Add conditional extraction for zip files
    Conditionally extract zip files only if acis.db does not exist.

--- api/index.php
+++ api/index.php
@@ -1,15 +1,17 @@
 <?php
 $datapath = dirname(__DIR__) . '/data';
-$zip = new ZipArchive;
-$zip->open("$datapath/acis.zip");
-$zip->extractTo('/tmp/');
-$zip->close();
-$zip->open("$datapath/spec.zip");
-$zip->extractTo('/tmp/');
-$zip->close();
+if (!file_exists('/tmp/acis.db')) {
+  $zip = new ZipArchive;
+  $zip->open("$datapath/acis.zip");
+  $zip->extractTo('/tmp/');
+  $zip->close();
+  $zip->open("$datapath/spec.zip");
+  $zip->extractTo('/tmp/');
+  $zip->close();
+}
 //$datapath = dirname(__DIR__) . '/macs/data';
 //copy("$datapath/acis.db", '/tmp/acis.db');
 //copy("$datapath/spec.db", '/tmp/spec.db');
-//chdir(dirname(__DIR__) . '/macs');
+chdir(dirname(__DIR__) . '/macs');
 require_once "./index.php";
 ?>
