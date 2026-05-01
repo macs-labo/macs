ecd06d4eaacf645368b1a83dfde5b60a827e5e0c
OhYeah! <hidemioya@gmail.com>
2026-05-01 10:05:57 +0900

    Fix syntax error in data path assignment

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 //chdir(dirname(__DIR__) . '/data');
 //echo exec("unzip acis.zip -d /tmp");
 //echo exec("unzip spec.zip -d /tmp");
-$datapath = dirname(__DIR__) . '/macs/data;
+$datapath = dirname(__DIR__) . '/macs/data';
 copy("$datapath/acis.db", '/tmp/acis.db');
 copy("$datapath/spec.db", '/tmp/spec.db');
 chdir(dirname(__DIR__) . '/macs');
