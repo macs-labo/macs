bf37058c817a6dd351f8f2501156fca708b9dd56
OhYeah! <hidemioya@gmail.com>
2026-05-01 10:05:24 +0900

    Update data directory path for database copies

--- api/index.php
+++ api/index.php
@@ -2,9 +2,9 @@
 //chdir(dirname(__DIR__) . '/data');
 //echo exec("unzip acis.zip -d /tmp");
 //echo exec("unzip spec.zip -d /tmp");
-$datadir = __DIR__ . '/data;
-copy("$datadir/acis.db", "/tmp/acis.db");
-copy("$datadir/spec.db", "/tmp/spec.db");
+$datapath = dirname(__DIR__) . '/macs/data;
+copy("$datapath/acis.db", '/tmp/acis.db');
+copy("$datapath/spec.db", '/tmp/spec.db');
 chdir(dirname(__DIR__) . '/macs');
 require_once "./index.php";
 ?>
