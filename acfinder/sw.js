10064a499f63677335990d3d0111145f982a12f2
OhYeah! <hidemioya@gmail.com>
2026-09-14 16:57:26 +0900

    Add files via upload

--- acfinder/sw.js
+++ acfinder/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'acfinder-assets-v5.68r79'; // バージョンを上げる
+const CACHE_NAME = 'acfinder-assets-v5.68r80'; // バージョンを上げる
 const ASSETS_TO_CACHE = [
 	'./crop.html',
 	'./pest.html',
@@ -64,7 +64,6 @@ const ASSETS_TO_CACHE = [
 	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/addon/hint/sql-hint.js',
 	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/addon/selection/active-line.js',
 	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/addon/search/searchcursor.js',
-	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/addon/search/search.js',
 	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/lib/codemirror.css',
 	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/theme/eclipse.css',
 	'https://cdn.jsdelivr.net/npm/codemirror@5.65.21/theme/darcula.css',
