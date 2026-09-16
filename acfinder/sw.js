02fc130da85250053a6899383e68188928ea8f25
OhYeah! <hidemioya@gmail.com>
2026-09-16 15:08:50 +0900

    Add files via upload

--- acfinder/sw.js
+++ acfinder/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'acfinder-assets-v5.68r131'; // バージョンを上げる
+const CACHE_NAME = 'acfinder-assets-v5.68r132'; // バージョンを上げる
 const ASSETS_TO_CACHE = [
 	'./crop.html',
 	'./pest.html',
