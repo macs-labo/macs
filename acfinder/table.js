bb362ee5cc8f2481f3879033bb53c86fb8bbc9fd
OhYeah! <hidemioya@gmail.com>
2026-09-20 19:56:06 +0900

    Add files via upload

--- acfinder/table.js
+++ acfinder/table.js
@@ -728,6 +728,7 @@ function outputTable(selector, result, option = {}) {
 		console.log('wtHolder_width: ', width);
 		const cloneTop = tableContainer.querySelector('.ht_clone_top');
 		const cloneHolder = cloneTop.querySelector('.wtHolder');
+		//const width = parseFloat(tableContainer.querySelector('.wtHolder')?.style.width);
 		const width = parseFloat(cloneHolder.style.width);
 		cloneTop.style.setProperty('width', `${width - overlayScrollbarWidth}px`, 'important');
 		cloneHolder.style.setProperty('width', `${width - overlayScrollbarWidth}px`, 'important');
