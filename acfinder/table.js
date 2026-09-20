890c0e5786a507db7959cc753cb818226ed59972
OhYeah! <hidemioya@gmail.com>
2026-09-20 18:53:42 +0900

    Add files via upload

--- acfinder/table.js
+++ acfinder/table.js
@@ -596,7 +596,7 @@ function outputTable(selector, result, option = {}) {
 		afterRender: function() {
 			adjustHeaderWidth(this);
 		},
-		afterScroolHolizontary: function() {
+		aftafterScrollHorizontally: function() {
 			adjustHeaderWidth(this);
 		},
 		afterDropdownMenuShow: function(dropdownMenu) {
